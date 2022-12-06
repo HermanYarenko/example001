a76f026b519ae98033e91523d14b752a8f6fd880
HermanYarenko <teyonel@icloud.com>
2022-12-06 17:34:01 +0300

    novor zadanie

---
+++ example000_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array ={19,2,3,4,19,5,6,7,8,9};
+
+int n = array.Length;
+int find =19;
+
+int index =0;
+
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index= index + 1;
+    index++;
+}
